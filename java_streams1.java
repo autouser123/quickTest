dafaa215576fe68a5c0e6d284d777c7705a71c24
autouser123 <manish.patidar88@gmail.com>
2019-03-16 16:20:31 +0530

    Update java_streams1.java

--- java_streams1.java
+++ java_streams1.java
@@ -18,12 +18,15 @@ public class java_streams1 {
 		al.add(60);
 		System.out.println(al);
 		
+		//Method1
 		List<Integer> list = al.stream().sorted((i1,i2)->(i1<i2)?1:(i1>i2)?-1:0).collect(Collectors.toList());
 		System.out.println(list);
 		//OR
+		//Method 2
 		List<Integer> list1 = al.stream().sorted((i1,i2)-> -i1.compareTo(i2)).collect(Collectors.toList());
 		System.out.println(list1);
 		//OR
+		//Method 3
 		List<Integer> list2 = al.stream().sorted((i1,i2)-> i2.compareTo(i1)).collect(Collectors.toList());
 		System.out.println(list2);
 	}
